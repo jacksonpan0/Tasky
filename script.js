6f5bfd8b605d9dee2361312139df76e8868a7517
pnjnlng <pan.jackson1@gmail.com>
2023-10-28 00:25:32 -0400

    Reorganize CSS and add Task Count, Limit Functions

--- script.js
+++ script.js
@@ -2,26 +2,41 @@
 const taskInput = document.getElementById('taskInput');
 const addTaskButton = document.getElementById('addTask');
 const taskList = document.getElementById('taskList');
-
+let taskLimit = 10;
+let taskAmount = 0;
 // Event listener for adding a new task
 addTaskButton.addEventListener('click', function() {
     const taskText = taskInput.value;
-    if (taskText.trim() !== '') {
-        addTask(taskText);
-        taskInput.value = '';
+    if(taskAmount < taskLimit) {
+        if(taskText.trim() !== '') {
+            addTask(taskText);
+            taskInput.value = '';
+        }
+        taskAmount++;
+        updateTaskCount();
     }
 });
 
 // Event listener for adding a new task when the "Enter" key is pressed in the text input
 taskInput.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        const taskText = taskInput.value;
-        if (taskText.trim() !== '') {
-        addTask(taskText);
-        taskInput.value = '';
-    }
+    if(event.key === 'Enter') {
+        if(taskAmount < taskLimit) {
+            const taskText = taskInput.value;
+            if(taskText.trim() !== '') {
+                addTask(taskText);
+                taskInput.value = '';
+            }
+        }
+        taskAmount++;
+        updateTaskCount();
     }
 });
+
+// Function to update the task count in the HTML
+function updateTaskCount() {
+    const taskCountElement = document.getElementById('taskCount');
+    taskCountElement.textContent = `Task Count: ${taskAmount}`;
+}
 
 // Function to add a new task
 function addTask(taskText) {
@@ -36,6 +51,8 @@ function addTask(taskText) {
     // Add event listener to delete button
     taskItem.querySelector('.delete').addEventListener('click', function() {
         taskList.removeChild(taskItem);
+        taskAmount--;
+        updateTaskCount();
     });
 
     // Add event listener to complete button
@@ -43,6 +60,8 @@ function addTask(taskText) {
         // Handle task completion here, e.g., change the style, move to a completed list, etc.
         // For now, let's just remove it
         taskList.removeChild(taskItem);
+        taskAmount--;
+        updateTaskCount();
     });
 }
 

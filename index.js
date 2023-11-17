8094e0bcf873a2af23ecbc4ad80725fbc06b9f66
pnjnlng <pan.jackson1@gmail.com>
2023-11-16 21:53:09 -0500

    new changes

--- index.js
+++ index.js
@@ -69,27 +69,6 @@ function markComplete(taskItem) {
     saveCompletedTask(taskItem.innerText);
 }
 
-// Save a completed task to localStorage
-function saveCompletedTask(taskText) {
-    let completedTasks = JSON.parse(localStorage.getItem('completedTasks')) || [];
-    completedTasks.push(taskText);
-    localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
+function goToCompletedTaskPage() {
+    window.location.href = "completed-task.html";
 }
-
-// Retrieve and display completed tasks
-function displayCompletedTasks() {
-    let completedTasks = JSON.parse(localStorage.getItem('completedTasks')) || [];
-
-    // Loop through completedTasks and display them in the HTML
-    completedTasks.forEach(function (taskText) {
-        const taskItem = document.createElement('li');
-        taskItem.textContent = taskText;
-        completedList.appendChild(taskItem);
-    });
-}
-
-window.addEventListener('load', function () {
-    if (window.location.href.endsWith('completed-tasks.html')) {
-        displayCompletedTasks();
-    }
-});
